2da8776a528724837d94a612698ba59f6b34bf48
serverside-swzo <omarahmedmsalem@gmail.com>
2025-05-03 20:00:22 +0300

    removed daily rewards and made first login rewards less

--- kubejs/server_scripts/first_reward.js
+++ kubejs/server_scripts/first_reward.js
@@ -4,8 +4,8 @@ PlayerEvents.loggedIn(event => {
     // Check if the player has already received the reward
     if (!player.persistentData.hasReceivedBevels) {
         player.persistentData.hasReceivedBevels = true; // Mark that the player has received the reward
-        player.give(Item.of('numismatics:bevel', 50)); // Give the player 50 Bevels
-        player.tell(Text.green("You have received 50 Bevels [250¤] as a first-time login reward!"));
-        console.info(`[KubeJS] Given 50 Bevels to ${player.name.string} on first login.`);
+        player.give(Item.of('numismatics:bevel', `16`)); 
+        player.tell(Text.green("You have received 16 Bevels as a first-time login reward!"));
+        console.info(`[KubeJS] Given 16 Bevels to ${player.name.string} on first login.`);
     }
 });
