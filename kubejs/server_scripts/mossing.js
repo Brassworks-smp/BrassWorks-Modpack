ca67cb53dbf11f3d9364f6df60c6c58533af5a99
serverside-swzo <omarahmedmsalem@gmail.com>
2025-05-04 01:12:58 +0300

    vertical slab for all mods, thats it lol

---
+++ kubejs/server_scripts/mossing.js
@@ -0,0 +1,55 @@
+// 1) Tag for deployer inputs
+ServerEvents.tags('item', event => {
+    event.add('kubejs:moss_deployer_inputs', [
+        'minecraft:moss_block',
+        'minecraft:vines'
+    ])
+})
+
+ServerEvents.recipes(event => {
+
+    const tools = [
+        'minecraft:moss_block',
+        'minecraft:vine'
+    ]
+
+    const recipes = [
+        { output: 'minecraft:mossy_cobblestone',    input: 'minecraft:cobblestone' },
+        { output: 'quark:mossy_cobblestone_bricks', input: 'quark:cobblestone_bricks' },
+        { output: 'minecraft:mossy_stone_bricks',   input: 'minecraft:stone_bricks' },
+        { output: 'createdeco:mossy_red_bricks',    input: 'minecraft:bricks' },          // special case
+        { output: 'createdeco:mossy_verdant_bricks',input: 'createdeco:verdant_bricks' },
+        { output: 'createdeco:mossy_umber_bricks',  input: 'createdeco:umber_bricks' },
+        { output: 'createdeco:mossy_pearl_bricks',  input: 'createdeco:pearl_bricks' },
+        { output: 'createdeco:mossy_scarlet_bricks',input: 'createdeco:scarlet_bricks' },
+        { output: 'createdeco:mossy_blue_bricks',   input: 'createdeco:blue_bricks' },
+        { output: 'createdeco:mossy_dusk_bricks',   input: 'createdeco:dusk_bricks' },
+        { output: 'createdeco:mossy_dean_bricks',   input: 'createdeco:dean_bricks' }
+    ]
+
+    tools.forEach(tool => {
+        const toolName = tool.split(':')[1]
+
+        recipes.forEach(recipe => {
+            const output = recipe.output
+            const input  = recipe.input
+            const base   = input.replace(':', '_')
+            const mossy  = output.split(':')[1]
+
+            const id = `kubejs:deploying/${base}_to_${mossy}_with_${toolName}`
+
+            if (typeof event.recipes.deploying === 'function') {
+                event.recipes.deploying(output, input, tool).id(id)
+            } else {
+                event.custom({
+                    type: 'create:deploying',
+                    ingredients: [
+                        { item: input },
+                        { item: tool }
+                    ],
+                    results: [{ item: output }]
+                }).id(id)
+            }
+        })
+    })
+})
