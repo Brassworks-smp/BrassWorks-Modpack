6cabb64a2b5c27d266e48878b20c9a91ff818b2d
serverside-swzo <omarahmedmsalem@gmail.com>
2025-05-04 21:08:07 +0300

    fixed the claim books name

--- kubejs/server_scripts/first_reward.js
+++ kubejs/server_scripts/first_reward.js
@@ -24,6 +24,8 @@ PlayerEvents.loggedIn(event => {
                   id:    "minecraft:written_book",
                   Count: 1,
                   tag: {
+                    HideFlags: 4,
+                    "quark:RuneColor": "red",
                     CustomModelData: 69420,
                     author: "swzo",
                     display: {
@@ -55,10 +57,12 @@ PlayerEvents.loggedIn(event => {
                     author: "swzo",
                     resolved: 1,
                     display: {
-                        Name: '[["",{"text":"🏰Landlord’s Ledger","italic":false,"color":"#cc9966"}]]',
+                        Name: '[["",{"text":"🏰 Landlord’s Ledger","italic":false,"color":"#cc9966"}]]',
                         Lore: ['["",{"text":"A Landlord’s Guide to the Real Estate Market","italic":false,"color":"#33cc66"}]']
                     },
                     CustomModelData: 696969,
+                    HideFlags: 4,
+                    "quark:RuneColor": "lime",
                     pages: [
                         '[["",{"text":"📌","color":"#c26251"},{"text":"Claiming Land 101","color":"#c26251","bold":true},"\\nBy default every newcomer gets ",{"text":"4 free chunks","color":"#ffcc33","bold":true},". Want more claim slots? Open the ",{"text":"OpenPAC","color":"#ff99cc"}," menu with ",{"text":"’","bold":true}," (apostrophe) or ",{"text":"click here","color":"#66cccc","clickEvent":{"action":"run_command","value":"/openguiscreen xaero.pac.client.gui.MainMenu"},"hoverEvent":{"action":"show_text","contents":"Open Claims GUI"}},". You’ll see:\\nA ",{"text":"slider","bold":true}," to increase your total claim limit\\nA ",{"text":"Buy Chunks","color":"#00cc66","bold":true}," button to pay with Spurs\\nEach extra chunk slot costs ",{"text":"4 Spurs","color":"#c26251","bold":true},".\\n",{"text":"(flip to next page)","color":"#666666"}]]',
                         '[["",{"text":"🔲","color":"#66cccc"},{"text":"Claiming Chunks","color":"#66cccc","bold":true},"\\nTo actually claim land:\\n",{"text":"Right-click","bold":true}," and drag over the chunks you want on the map.\\nClick ",{"text":"Claim Chunks","color":"#00cc66","bold":true},".\\nEach claimed chunk uses one of your available slots. Only you and your party can build there.\\n",{"text":"(flip to next page)","color":"#666666"}]]',
