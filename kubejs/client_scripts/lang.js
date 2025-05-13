f264d4ebebb6b60890c2dfe1b2743a889581e5e3
serverside-swzo <omarahmedmsalem@gmail.com>
2025-05-13 20:10:14 +0300

    added mobile packages!!! and connected glass

---
+++ kubejs/client_scripts/lang.js
@@ -0,0 +1,3 @@
+ClientEvents.lang('en_us', e => {
+  e.add('block.createenchantablemachinery.enchantable_crushing_wheel_controller', 'Crushing Wheel Controller')
+})
--- kubejs/server_scripts/miscrecipes.js
+++ kubejs/server_scripts/miscrecipes.js
@@ -3,6 +3,8 @@ ServerEvents.recipes(event => {
   event.remove({ output: 'numismatics:bank_terminal' })
   event.remove({ output: 'supplementaries:cannon' })
   event.remove({ output: 'buzzier_bees:honey_apple' })
+  event.remove({ output: 'vanillabackport:dried_ghast' })
+  event.remove({ id: 'minecraft:lead' }) // Remove vanilla lead recipe
 
   // New shapeless Bank Terminal recipe
   event.shapeless(
@@ -31,6 +33,42 @@ ServerEvents.recipes(event => {
     ]
   )
 
+  // Fern Recipe (seeds + short grass)
+  event.shapeless(
+    'minecraft:fern',
+    [
+      '#forge:seeds',
+      'minecraft:grass'
+    ]
+  )
+
+  // Large Fern Recipe (2 ferns)
+  event.shapeless(
+    'minecraft:large_fern',
+    [
+      '2x minecraft:fern'
+    ]
+  )
+
+  // backport of new lead recipe from 1.21.6 
+  event.shaped('2x minecraft:lead', [
+    'SS ',
+    'SS ',
+    '  S'
+  ], {
+    S: 'minecraft:string'
+  })
+
+  // backport of dried ghast recipe from 1.21.6
+  event.shaped('vanillabackport:dried_ghast', [
+    'TTT',
+    'TST',
+    'TTT'
+  ], {
+    T: 'minecraft:ghast_tear',
+    S: 'minecraft:soul_sand'
+  })
+
   // whoopee cushion
   event.shaped('artifacts:whoopee_cushion', [
     ' L ',
@@ -51,6 +89,17 @@ ServerEvents.recipes(event => {
   ], {
     P: '#minecraft:planks',
     M: '#minecraft:music_discs'
+  })
+
+  // Sniffer Egg Recipe
+  event.shaped('minecraft:sniffer_egg', [
+    'NMN',
+    'MEM',
+    'NMN'
+  ], {
+    N: 'minecraft:nether_wart_block',
+    M: 'minecraft:moss_block',
+    E: '#forge:eggs'
   })
 
   // Smelt Bank Terminal into a Spur
