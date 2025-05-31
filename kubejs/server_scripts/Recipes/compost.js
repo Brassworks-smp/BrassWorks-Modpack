1e61414009bd219c64fb517bb62ff8a1a1517fdc
serverside-swzo <omarahmedmsalem@gmail.com>
2025-05-31 18:35:04 +0300

    s

---
+++ kubejs/server_scripts/Recipes/compost.js
@@ -0,0 +1,5 @@
+   ServerEvents.compostableRecipes(event => {
+    event.add('quark:moss_paste', 1.0)
+    }) 
+    // better bonemeal farming since moss paste is guaranteed to be 
+    // composted while moss is only a 65% chance
