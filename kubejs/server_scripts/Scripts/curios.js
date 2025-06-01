2724180bd129b4976f93d70d9b3a6dc6e68e3706
serverside-swzo <omarahmedmsalem@gmail.com>
2025-06-01 10:58:36 +0300

    removed scoreboard overhaul, and moved some mods from curse to modrinth

--- kubejs/server_scripts/Scripts/curios.js
+++ kubejs/server_scripts/Scripts/curios.js
@@ -4,15 +4,27 @@
 ServerEvents.tags('item', event => {
     // Add all size variants of filling tanks to the belt tag
     ['small', 'medium', 'large'].forEach(size => {
-      event.add('curios:belt', `create_sa:${size}_filling_tank`);
+      event.add('curios:hands', `create_sa:${size}_filling_tank`);
       event.add('curios:charm', `create_sa:${size}_filling_tank`);
     });
 
     
     // Add all size variants of fueling tanks to the belt tag
     ['small', 'medium', 'large'].forEach(size => {
-      event.add('curios:belt', `create_sa:${size}_fueling_tank`);
+      event.add('curios:hands', `create_sa:${size}_fueling_tank`);
       event.add('curios:charm', `create_sa:${size}_fueling_tank`);
     });
   });
-  
+
+// Move all 'artifacts' items in #curios:hands to curios:belt with high priority
+ServerEvents.tags('item', { priority: 'high' }, event => {
+  // Collect all items in #curios:hands at the start
+  const allHandsItems = event.get('curios:hands').getObjectIds().map(id => id.toString());
+  // Filter for artifacts items
+  const artifactsHands = allHandsItems.filter(id => id.startsWith('artifacts:'));
+  // Add to belt
+  artifactsHands.forEach(id => event.add('curios:belt', id));
+  // Remove from hands
+  artifactsHands.forEach(id => event.remove('curios:hands', id));
+});
+
