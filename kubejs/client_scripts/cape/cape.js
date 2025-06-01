b128f4d7f898ce7aa5ce95b0f14b9bf08ff21320
serverside-swzo <omarahmedmsalem@gmail.com>
2025-06-01 23:27:35 +0300

    updated cape script to allow new json format

--- kubejs/client_scripts/cape/cape.js
+++ kubejs/client_scripts/cape/cape.js
@@ -1,9 +1,12 @@
+let gists_id = '6e223cae5b157ebba1e31da7d14c2d16';
+
 CapeJS.addCapes(event => {
-    // Read the UUIDs and cape types from the JSON file
+    // Read the UUIDs and cape info from the JSON file
     let capeData = JsonIO.read('kubejs/assets/capedb.json');
     if (capeData) {
         for (let uuid in capeData) {
-            let capeType = capeData[uuid];
+            let info = capeData[uuid];
+            let capeType = info.cape;  // Access cape property
             event.register(uuid, capeType);
         }
     }
