0ab84b3d133ec9f04764c22f636e38df3cb666f0
serverside-swzo <omarahmedmsalem@gmail.com>
2025-06-02 21:57:19 +0300

    fixed a bug with elytra firework removal and nerfed jetpack speed even more

--- kubejs/server_scripts/Scripts/noelytra.js
+++ kubejs/server_scripts/Scripts/noelytra.js
@@ -1,8 +1,9 @@
 ItemEvents.rightClicked(event => {
-    const { item, player, hand } = event;
+    const { item, player } = event;
     const chestItem = player.inventory.getArmor(2);
     if (!chestItem || chestItem.id !== 'minecraft:elytra') return;
-    if (!item || hand !== 'main_hand' || item.id !== 'minecraft:firework_rocket') return;
+    if (!item || item.id !== 'minecraft:firework_rocket') return;
+    // Trigger for both hands
     player.tell('\u00A7cElytra Firework Boosting is disabled for balancing reasons! Please use Immersive Aircrafts or Trains instead.');
     event.cancel();
 });
